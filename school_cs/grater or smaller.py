112c24172f4978ca3bc55a4254ea0570153ba45f
Aksaykanthan <86051039+Aksaykanthan@users.noreply.github.com>
2022-02-03 21:41:10 +0530

    new

--- school_cs/grater or smaller.py
+++ school_cs/grater or smaller.py
@@ -1,12 +1,12 @@
 
-num_1 = input("enter a num1  ")
+n1 = input("enter the ")
+num_2 = input("enter the ")
 
-num_2 = input("enter a num2  ")
 
-if num_1 > num_2:
-    print(num_1 + " is greater than " + num_2)
-elif num_2 > num_1:
-    print(num_2 + " is greater than " + num_1)
-elif num_1 == num_2:
-    print(num_2 +" and " + num_1 + " is  equal ")
+if n1 > num_2:
+    print(n1 + " is greater than " + num_2)
+elif num_2 > n1:
+    print(num_2 + " is greater than " + n1)
+elif n1 == num_2:
+    print(num_2 +" and " + n1 + " is  equal ")
 
